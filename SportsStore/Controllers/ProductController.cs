dd083434c72eea8a57ac770e04965b0ad944b2e5
Sergey Simakin <sergey.simakin98@gmail.com>
2020-01-18 02:18:38 +0200

    add view and controller for product

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
