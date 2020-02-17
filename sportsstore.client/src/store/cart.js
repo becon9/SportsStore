5118f6844b1f75d5e880815f8bbcc15a7a7dfb7d
Sergey Simakin <sergey.simakin98@gmail.com>
2020-02-17 23:31:05 +0200

    complete cart

--- sportsstore.client/src/store/cart.js
+++ sportsstore.client/src/store/cart.js
@@ -9,21 +9,43 @@ export default {
     },
     mutations: {
         addProduct(state, product) {
-            let line = state.lines.find(line => line.product.productId == product.productId);
-            if(line != null) {
+            let line = state.lines.find(line => line.product.productId === product.productId);
+            if (line != null) {
                 line.quantity++;
             } else {
-                state.lines.push({product: product, quantity: 1});
+                state.lines.push({ product: product, quantity: 1 });
             }
         },
         changeQuantity(state, update) {
             update.line.quantity = update.quantity;
         },
         removeProduct(state, lineToRemove) {
-            let index = state.lines.findIndex(line => line == lineToRemove);
-            if(index > -1 ) {
+            let index = state.lines.findIndex(line => line === lineToRemove);
+            if (index > -1) {
                 state.lines.splice(index, 1);
             }
+        },
+        setCartData(state, data) {
+            state.lines = data;
+        }
+    },
+    actions: {
+        loadCartData(context) {
+            let data = localStorage.getItem("cart");
+            if (data != null) {
+                context.commit("setCartData", JSON.parse(data));
+            }
+        },
+        storeCartData(context) {
+            localStorage.setItem("cart", JSON.stringify(context.state.lines));
+        },
+        clearCartData(context) {
+            context.commit("setCartData", []);
+        },
+        initializeCart(context, store) {
+            context.dispatch("loadCartData");
+            store.watch(state => state.cart.lines,
+                () => context.dispatch("storeCartData"), {deep: true});
         }
     }
 }
